99abe4f90890c4b15eb4c573663ce1ce916524ca
Jan Buschtöns <buschtoens@gmail.com>
2017-12-28 14:39:13 +0100

    Upgrade all the things (#197)
    * chore(deps): upgrade all dependencies
    
    * chore: apply newest `ember init` diff
    
    This also removes `useLinteTree` from the `ember-cli-qunit` config, which seems to be defunct.
    
    * chore(package & ci): switch back to Node >= 6

--- testem.js
+++ testem.js
@@ -3,21 +3,27 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Firefox',
-    'Chrome'
+    'Chrome',
+    'Firefox'
   ],
   launch_in_dev: [
     'Chrome'
   ],
-  'browser_args': {
-    'Chrome': [
-      '--touch-events',
-      '--disable-gpu',
-      '--headless',
-      '--remote-debugging-port=9222'
-    ],
-    'Firefox': [
-      '-headless'
-    ]
+  browser_args: {
+    Chrome: {
+      mode: 'ci',
+      args: [
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ]
+    },
+    Firefox: {
+      mode: 'ci',
+      args: [
+        '-headless'
+      ]
+    }
   }
 };
