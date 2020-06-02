8bd4a549124a14cdef043a84b2085391d75e5d92
Jiří Prokop <jprokop@synaptiko.cz>
2020-06-02 17:54:34 +0200

    v4.0.0-rc.4-bayzat.1

--- addon/components/fa-icon/component.js
+++ addon/components/fa-icon/component.js
@@ -1,90 +1,33 @@
 import Component from '@ember/component';
-
-import { argument } from '@ember-decorators/argument';
-import { type, unionOf, optional } from '@ember-decorators/argument/type';
-import { ClosureAction } from '@ember-decorators/argument/types';
-
 import layout from './template';
 
-export default class FaIconComponent extends Component.extend({
+let FaIconComponent = Component.extend({
   layout,
-  tagName: ''
-}) {
-  static positionalParams = ['icon'];
+  tagName: '',
 
-  @argument
-  @type('string')
-  icon;
+  icon: null,
+  id: null,
+  class: null,
+  title: null,
+  ariaLabel: null,
+  color: null,
+  click: null,
+  size: null,
+  pull: null,
+  rotate: null,
+  flip: null,
+  stack: null,
+  ariaHidden: true,
+  fixedWidth: false,
+  pulse: false,
+  inverse: false,
+  border: false,
+  spin: false,
+  listItem: false
+});
 
-  @argument
-  @type(optional('string'))
-  id;
+FaIconComponent.reopenClass({
+  positionalParams: ['icon']
+});
 
-  @argument
-  @type(optional('string'))
-  class;
-
-  @argument
-  @type(optional('string'))
-  title;
-
-  @argument
-  @type(optional('string'))
-  ariaLabel;
-
-  @argument
-  @type('boolean')
-  ariaHidden = true;
-
-  @argument
-  @type(optional('string'))
-  color;
-
-  @argument
-  @type(optional(ClosureAction))
-  click;
-
-  @argument
-  @type(optional(unionOf('number', 'string')))
-  size;
-
-  @argument
-  @type(optional('string'))
-  pull;
-
-  @argument
-  @type(optional('number'))
-  rotate;
-
-  @argument
-  @type(optional('string'))
-  flip;
-
-  @argument
-  @type(optional('number'))
-  stack;
-
-  @argument
-  @type('boolean')
-  fixedWidth = false;
-
-  @argument
-  @type('boolean')
-  pulse = false;
-
-  @argument
-  @type('boolean')
-  inverse = false;
-
-  @argument
-  @type('boolean')
-  border = false;
-
-  @argument
-  @type('boolean')
-  spin = false;
-
-  @argument
-  @type('boolean')
-  listItem = false;
-}
+export default FaIconComponent;
